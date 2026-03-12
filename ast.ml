22b4150997c8a9a3670ae04b8422c4b8f449b28e
Sebastian-Jensen1 <hm12nx@student.aau.dk>
2026-03-12 22:44:09 +0100

    made a simple parser,lexer,ast and main file to run the whole pipeline for domain.pddl

--- ast.ml
+++ ast.ml
@@ -1,13 +1,26 @@
-type symbol = {
-	name : string;
-}
+
 
 type define = { domain_name : string;}
 
+
 type requirements = {	features : string list;}
+
+(* This is our 'main' type. we need to put all the rest of the types in here*)
+type program = {defs : define; main : requirements}
+(*
+type symbol = {name : string;}
 
 type predicates = {
   pred_name : string;
 	pred_params : symbol list;
+} 
+
+type derived = { 
+  derived_conditions : predicates;
+  derived_stmt : string list;
 }
+
+type stmt = {
+  name : string list;
+} *)
 
--- p4.ml
+++ p4.ml
@@ -1,2 +1,15 @@
-(* Without this file, ast kept giving errors*)
-let () = ()
+let () =
+  (* opens a little snippet of the domain.pddl *)
+  let filename = "domain-mini.pddl" in
+  (* opens the file and creates a lexer buffer. *)
+  let input_channel = open_in filename in
+  let lexbuf = Lexing.from_channel input_channel in
+  (* it parses using the parser entrypoint and lexer token . *)
+  match Parser.prog Lexer.token lexbuf with
+  | result ->
+    Printf.printf "Parsed domain: %s\n" result.defs.domain_name;
+    Printf.printf "Requirements: %s\n" (String.concat ", " result.main.features);
+    close_in input_channel
+  | exception Failure msg ->
+    Printf.printf "Parse error: %s\n" msg;
+    close_in input_channel
