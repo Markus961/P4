0a46e9c4bb50d394d9a7c1b7132d28b3a0a9f94f
Sebastian-Jensen1 <hm12nx@student.aau.dk>
2026-03-12 12:55:04 +0100

    started on ast file. created p4 file, otherwise te ast file gave error

---
+++ ast.ml
@@ -0,0 +1,13 @@
+type symbol = {
+	name : string;
+}
+
+type define = { domain_name : string;}
+
+type requirements = {	features : string list;}
+
+type predicates = {
+  pred_name : string;
+	pred_params : symbol list;
+}
+
