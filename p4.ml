0aed098397def2356dec67440a27d2094532394e
Patrickdh04 <163329805+Patrickdh04@users.noreply.github.com>
2026-03-24 11:16:54 +0100

    Small update

--- p4.ml
+++ p4.ml
@@ -11,22 +11,27 @@ let string_of_predicate = function
       Printf.sprintf "(%s)" pname
   | { pname; variables } ->
       Printf.sprintf "(%s %s)" pname (String.concat " " variables)
-(*
-let string_of_derived = function
-| Epdef p -> Printf.sprintf "(%s)" string_of_predicate defs.derived.header.p
-*)
+      
+(*let string_of_derived = function
+  | { pdef; logic = [] } ->
+    Printf.sprintf "(%s)" pdef
+  | { pdef; logic } ->
+      Printf.sprintf "(%s %s)" pdef (String.concat " " logic)*)
+
 let () =
   (* opens a little snippet of the domain.pddl *)
-  let filename = "domain-mini.pddl" in
+  let filename = "problem.pddl" in
   (* opens the file and creates a lexer buffer. *)
   let input_channel = open_in filename in
   let lexbuf = Lexing.from_channel input_channel in
   (* it parses using the parser entrypoint and lexer token . *)
   match Parser.prog Lexer.token lexbuf with
   | result ->
-    Printf.printf "(define (domain %s)\n" result.defs.domain.domain_name;
-    Printf.printf "(:requirements %s)\n" (String.concat " " (List.map string_of_feature result.defs.requirements.features));
-    Printf.printf "(:predicates %s\n" (String.concat "\n " (List.map string_of_predicate result.defs.predicates));
+    (*Printf.printf "Parsed domain: %s\n" result.defs.domain.domain_name;
+    Printf.printf "Requirements: %s\n" (String.concat " " (List.map string_of_feature result.defs.requirements.features));
+    Printf.printf "Predicates: %s\n" (String.concat "\n " (List.map string_of_predicate result.defs.predicates));
+    Printf.printf "Derived: %s\n"  (String.concat "\n " (List.map string_of_predicate result.defs.derived));*)
+
     close_in input_channel
   | exception Failure msg ->
     Printf.printf "Parse error: %s\n" msg;
