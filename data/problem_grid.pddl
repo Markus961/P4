cd1ea6768f19825f601ee6f231f5d748077e5b4a
Jakob <jdj1994@gmail.com>
2026-05-10 15:21:58 +0200

    fixed test on connections

--- data/problem_grid.pddl
+++ data/problem_grid.pddl
@@ -4,22 +4,24 @@
    (:objects test_object)
 
    (:grid 
-      :rows 2
-      :columns 6
+      :rows 3
+      :columns 2
       :name fileno
-      :connections -H -V
+      :connections -H
       :keys key0 key1
       :shapes ((St = star))
       
       :lockedlocations fileno ([
-            [0]*3 + [1]*3
-            [0]*3 + [1]*3
+            [0]*2
+            [0]*2
+            [0]*2
             
             ] (shape triangle))
 
       :keylocations fileno ([
-            [St 0 0 0 0 0]
-            [St 0 0 0 0 0]
+            [St 0]
+            [St 0]
+            [0 0]
             ])
    )
 
--- test/ast_transformer_tests/test_generate_vertical.ml
+++ test/ast_transformer_tests/test_generate_vertical.ml
@@ -8,19 +8,34 @@ let test_generate_vertical () =
       {sname = "conn"; arguments = [
         OnlyArguments {a = "node0-0"}; 
         OnlyArguments {a = "node1-0"}]};
-
+    OnlyStates 
+      {sname = "conn"; arguments = [
+        OnlyArguments {a = "node1-0"}; 
+        OnlyArguments {a = "node0-0"}]};
     OnlyStates
       {sname = "conn"; arguments = [
         OnlyArguments {a = "node0-1"}; 
         OnlyArguments {a = "node1-1"}]}; 
+    OnlyStates
+      {sname = "conn"; arguments = [
+        OnlyArguments {a = "node1-1"}; 
+        OnlyArguments {a = "node0-1"}]};
     OnlyStates 
       {sname = "conn"; arguments = [
         OnlyArguments {a = "node1-0"}; 
         OnlyArguments {a = "node2-0"}]};
     OnlyStates 
       {sname = "conn"; arguments = [
+        OnlyArguments {a = "node2-0"}; 
+        OnlyArguments {a = "node1-0"}]};
+    OnlyStates 
+      {sname = "conn"; arguments = [
         OnlyArguments {a = "node1-1"}; 
         OnlyArguments {a = "node2-1"}]};
+    OnlyStates 
+      {sname = "conn"; arguments = [
+        OnlyArguments {a = "node2-1"}; 
+        OnlyArguments {a = "node1-1"}]};
   ]
   in
 
