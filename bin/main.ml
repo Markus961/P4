0cd55347fb137bab715cfcdb8010229c722f1089
Jakob <jdj1994@gmail.com>
2026-05-14 07:54:49 +0200

    fixed merge conflict in main file

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,7 @@
 
 let () =
   Compiler.process_file "./data/domain.pddl";
-<<<<<<< Inferred-Predicates+Readme
-  Compiler.process_file "./data/problem_grid.pddl";
-=======
   Compiler.process_file "./data/problem_gridindefine.pddl";
->>>>>>> main
 
   Planner.check_planner_installed ();
   Planner.run_planner ()
